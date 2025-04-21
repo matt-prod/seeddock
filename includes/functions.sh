606650f5bc1ec9e8fbfccf5b47dcdff1ae0c81ca
Matt <Matt@SeedDock>
2025-04-21 20:01:16 +0200

    correct paths

--- includes/functions.sh
+++ includes/functions.sh
@@ -139,10 +139,11 @@ deploy_sdm_container() {
   docker run -d --name sdm \
     --restart unless-stopped \
     --network traefik \
-    -v "${INSTALL_DIR}/SDM:/srv/sdm" \
-    -v "${INSTALL_DIR}/includes:/srv/sdm/includes" \
     -v "${INSTALL_DIR}/SDM/group_vars:/srv/sdm/group_vars" \
-    -v "${CONFIG_DIR}/vault_pass:/vault_pass" \
+    -v "${INSTALL_DIR}/SDM/config:/srv/sdm/config" \
+    -v "${INSTALL_DIR}/SDM/inventory:/srv/sdm/inventory" \
+    -v "${INSTALL_DIR}/SDM/playbooks:/srv/sdm/playbooks" \
+    -v "${INSTALL_DIR}/includes:/srv/sdm/includes" \
     -l "traefik.enable=true" \
     -l "traefik.http.routers.sdm.rule=PathPrefix(\`/\`)" \
     -l "traefik.http.routers.sdm.entrypoints=websecure" \
